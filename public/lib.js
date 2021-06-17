64bf550f78d634545f9592ddb3e0d376991b5a59
Savannaha <savwiley@yahoo.com>
2021-06-17 15:10:09 -0500

    removed firebase funcs

--- public/lib.js
+++ public/lib.js
@@ -1,17 +1,4 @@
-const firebase = require("firebase");
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCcPq3J_Yeex39izUWyerDoNRPan9XWwJw",
-  authDomain: "library-79cdb.firebaseapp.com",
-  projectId: "library-79cdb",
-  storageBucket: "library-79cdb.appspot.com",
-  messagingSenderId: "603775734043",
-  appId: "1:603775734043:web:47b551a4110cb35f2143be",
-  measurementId: "G-CGHSMSDCVY"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+import firebase from "./firebase";
 
 
 
